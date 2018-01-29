b84a44bc0afb911c0ea63b6ed43eeafe514b0a2c
Joshua Wilson <jwilson@waystone.systems>
2018-01-29 09:47:04 -0500

    Project moved to github and packaged for demo release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Climate By Forest
+
+This interactive graph shows modeled RCP 8.5 and RCP 4.5 data for forest ecoregions. This project is a fork of Climate By Location (a project for the NOAA Climate Program Office) for the US Forest Service.
+
+## Methodology and sources
+Forest climate data is selected by bounding box from the pre-gridded LOCA and Livneh datasets.
+ 
+* Downscaled modeled data: [LOCA](http://loca.ucsd.edu/).
+* Historical observed data: [Livneh](https://www.esrl.noaa.gov/psd/data/gridded/data.livneh.metvars.html).
+* Data services:  [Applied Climate Information System (ACIS)](http://www.rcc-acis.org/index.html).
+ 
+ 
+ 
+
--- demo.js
+++ demo.js
@@ -3,62 +3,61 @@ var cwg = undefined;
 $(document).ready(function () {
 
     function populate_variables(frequency) {
-        var variables = climate_widget.variables(frequency);
-        $("select#variable").empty();
-        $(variables.map(function (v) {
-            return ('<option value="' + v.id + '"' + '>' + v.title + '</option>');
-        }).join("")).appendTo($("select#variable"));
+      var variables = climate_widget.variables(frequency);
+      $("select#variable").empty();
+      $(variables.map(function (v) {
+        return ('<option value="' + v.id + '"' + '>' + v.title + '</option>');
+      }).join("")).appendTo($("select#variable"));
     }
 
     function initGraph() {
-        console.log()
-        cwg = climate_widget.graph({
-            'div': "div#widget",
-            'dataprefix': 'http://climate-widget-data.nemac.org/data',
-            'font': 'Roboto',
-            'frequency': $('#frequency').val(),
-            'timeperiod': $('#timeperiod').val(),
-            'bbox': $('#bbox').val(),
-            'variable': $('#variable').val(),
-            'scenario': $('#scenario').val(),
-            'xrangefunc': xrangeset,
-            'pmedian': true,
-            'hmedian': true
+      cwg = climate_widget.graph({
+        'div': "div#widget",
+        'dataprefix': 'http://climate-widget-data.nemac.org/data',
+        'font': 'Roboto',
+        'frequency': $('#frequency').val(),
+        'timeperiod': $('#timeperiod').val(),
+        'bbox': $('#forest').val(),
+        'variable': $('#variable').val(),
+        'scenario': $('#scenario').val(),
+        'xrangefunc': xrangeset,
+        'pmedian': true,
+        'hmedian': true
+      });
+      $('#forest').off().change(function () {
+        cwg.update({
+          bbox: $('#forest').val()
         });
-        $('#bbox').off().change(function () {
-            cwg.update({
-                bbox: $('#bbox').val()
-            });
-        });
-        $(window).resize(function () {
-            cwg.resize();
-        });
+      });
+      $(window).resize(function () {
+        cwg.resize();
+      });
     }
 
-    $('#bbox').change(initGraph);
+    $('#forest').change(initGraph);
 
 
     function update_frequency_ui() {
-        var freq = $('#frequency').val();
-        if (freq === "annual") {
-            $('#timeperiod-wrapper').hide();
-            $('#slider-range').show();
-            $('#x-axis-pan-note').hide();
-            $('#download_hist_mod_data_li').show();
-        }
-        if (freq === "monthly") {
-            $('#timeperiod-wrapper').show();
-            $('#slider-range').hide();
-            $('#x-axis-pan-note').show();
-            $('#download_hist_mod_data_li').hide();
-        }
-        if (freq === "seasonal") {
-            $('#timeperiod-wrapper').show();
-            $('#slider-range').hide();
-            $('#x-axis-pan-note').show();
-            $('#download_hist_mod_data_li').hide();
-        }
-        populate_variables(freq);
+      var freq = $('#frequency').val();
+      if (freq === "annual") {
+        $('#timeperiod-wrapper').hide();
+        $('#slider-range').show();
+        $('#x-axis-pan-note').hide();
+        $('#download_hist_mod_data_li').show();
+      }
+      if (freq === "monthly") {
+        $('#timeperiod-wrapper').show();
+        $('#slider-range').hide();
+        $('#x-axis-pan-note').show();
+        $('#download_hist_mod_data_li').hide();
+      }
+      if (freq === "seasonal") {
+        $('#timeperiod-wrapper').show();
+        $('#slider-range').hide();
+        $('#x-axis-pan-note').show();
+        $('#download_hist_mod_data_li').hide();
+      }
+      populate_variables(freq);
     }
 
     update_frequency_ui();
@@ -66,109 +65,108 @@ $(document).ready(function () {
 
     $('#frequency').change(function () {
 
-        update_frequency_ui();
-        if (cwg) {
-            cwg.update({
-                frequency: $('#frequency').val(),
-                variable: $('#variable').val()
-            });
-        }
+      update_frequency_ui();
+      if (cwg) {
+        cwg.update({
+          frequency: $('#frequency').val(),
+          variable: $('#variable').val()
+        });
+      }
     });
 
     $('#timeperiod').change(function () {
-        if (cwg) {
-            cwg.update({
-                timeperiod: $('#timeperiod').val()
-            });
-        }
+      if (cwg) {
+        cwg.update({
+          timeperiod: $('#timeperiod').val()
+        });
+      }
     });
 
     $('#variable').change(function () {
-        if (cwg) {
-            cwg.update({
-                variable: $('#variable').val()
-            });
-        }
+      if (cwg) {
+        cwg.update({
+          variable: $('#variable').val()
+        });
+      }
     });
     $('#rcp85, #rcp45').change(function () {
-        if (cwg) {
-            var scenario;
-            if ($('#rcp85').prop('checked')) {
-                if ($('#rcp45').prop('checked')) {
-                    scenario = 'both';
-                } else {
-                    scenario = 'rcp85';
-                }
-            } else if ($('#rcp45').prop('checked')) {
-                scenario = 'rcp45';
-            }
-            else {
-                scenario = '';
-            }
-            cwg.update({
-                scenario: scenario
-            });
+      if (cwg) {
+        var scenario;
+        if ($('#rcp85').prop('checked')) {
+          if ($('#rcp45').prop('checked')) {
+            scenario = 'both';
+          } else {
+            scenario = 'rcp85';
+          }
+        } else if ($('#rcp45').prop('checked')) {
+          scenario = 'rcp45';
         }
+        else {
+          scenario = '';
+        }
+        cwg.update({
+          scenario: scenario
+        });
+      }
     });
     $('#download-button').click(function () {
-        if (cwg) {
-            $('#download-panel').removeClass("hidden");
-        }
+      if (cwg) {
+        $('#download-panel').removeClass("hidden");
+      }
     });
 
     $('#download-dismiss-button').click(function () {
-        $('#download-panel').addClass("hidden");
+      $('#download-panel').addClass("hidden");
     });
 
     // download hooks
     $('#download-image-link').click(function () {
-        if (cwg) {
-            cwg.download_image(this)
-        }
+      if (cwg) {
+        cwg.download_image(this)
+      }
     });
     $('#download_hist_obs_data').click(function () {
-        if (cwg) {
-            cwg.download_hist_obs_data(this)
-        }
+      if (cwg) {
+        cwg.download_hist_obs_data(this)
+      }
     });
     $('#download_hist_mod_data').click(function () {
-        if (cwg) {
-            cwg.download_hist_mod_data(this)
-        }
+      if (cwg) {
+        cwg.download_hist_mod_data(this)
+      }
     });
     $('#download_proj_mod_data').click(function () {
-        if (cwg) {
-            cwg.download_proj_mod_data(this)
-        }
+      if (cwg) {
+        cwg.download_proj_mod_data(this)
+      }
     });
 
 
     $("#slider-range").slider({
-        range: true,
-        min: 1950,
-        max: 2099,
-        values: [1950, 2099],
-        slide: function (event, ui) {
-            // return the return value returned by setXRange, to keep
-            // the slider thumb(s) from moving into a disallowed range
-            return cwg.setXRange(ui.values[0], ui.values[1]);
-        }
+      range: true,
+      min: 1950,
+      max: 2099,
+      values: [1950, 2099],
+      slide: function (event, ui) {
+        // return the return value returned by setXRange, to keep
+        // the slider thumb(s) from moving into a disallowed range
+        return cwg.setXRange(ui.values[0], ui.values[1]);
+      }
     });
 
     // This function will be called whenever the user changes the x-scale in the graph.
     function xrangeset(min, max) {
-        // Force the slider thumbs to adjust to the appropriate place
-        $("#slider-range").slider("option", "values", [min, max]);
+      // Force the slider thumbs to adjust to the appropriate place
+      $("#slider-range").slider("option", "values", [min, max]);
     }
 
     WebFont.load({
-        google: {
-            families: ['Pacifico', 'Roboto']
-        }
+      google: {
+        families: ['Pacifico', 'Roboto']
+      }
     });
 
 
-});
+  }
+);
 
-//--------------------------------------------------------------------------
-var newcounties = []
