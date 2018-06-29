13758e2b8c01d4816b88a3cc3b427fdc0b1eebb4
Joshua Wilson <jwilson@waystone.systems>
2018-06-29 17:43:31 -0400

    re-processed ecoregion bounding boxes and added additional information for multi-part ecoregions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import pandas as pd
 import requests
 import click
 
@@ -9,11 +10,16 @@ import click
               help='Original Shapefile Geometries.')
 def find_bounding_box(shapefile):
     gdf = gpd.read_file(shapefile)
-    with open("output.txt", 'w') as file:
-        for index, row in gdf.iterrows():
-            foo = f'{row["geometry"].bounds}'.replace(" ", "")
-            file.write(f'<option value = "{foo}"> {row["MAP_UNIT_N"]}</option>\n')
-
+    gdf = gdf[~pd.isnull(gdf['FORESTNAME'])]
+    x = gdf[gdf['MAP_UNIT_N'].duplicated(keep=False)].copy()
+    x['ecoregion'] = x['MAP_UNIT_N'].str.cat(x['MAP_UNIT_S'], sep=" ")
+    y = gdf[~gdf['MAP_UNIT_N'].duplicated(keep=False)].copy()
+    y['ecoregion'] = y['MAP_UNIT_N']
+    z = pd.concat([y, x])
+    z['bbox'] = z['geometry'].apply(lambda a: str(a.bbox).replace(" ", "").replace("(", "").replace(")", ""))
+    z[['bbox', 'ecoregion', 'FORESTNAME']].copy()
+    z.groupby(by='FORESTNAME').apply(lambda b: b[['ecoregion', 'bbox']].sort_values(by='ecoregion').to_json(orient="records")).to_json('output.json', orient='index')
+    # after this you'll have to un-escape the inner json arrays
 
 if __name__ == '__main__':
     find_bounding_box()
